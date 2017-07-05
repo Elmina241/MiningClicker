8c8e117b3546f4a30a3b950bf478db1aec2fe554
Elmina Ross <Elmina Ross>
2017-07-05 21:20:53 +1000

    main script

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Game : MonoBehaviour {
+
+    private int money = 0;
+    private int score = 0;
+    private int bonus = 1;
+    public Text scoreText;
+    public int maxScore = 10;
+    public Text bonusText;
+    public Image p;
+    // Use this for initialization
+
+    public void OnClick () {
+        money += bonus;
+        score ++;
+        scoreText.text = money.ToString() + "$";
+        if (score > maxScore)
+        {
+            score = 0;
+        }
+        p.fillAmount = (float)score/(float)maxScore;
+    }
+    public void GetBonus()
+    {
+        money = money - bonus;
+        bonus = bonus + 2;
+        scoreText.text = money.ToString() + "$";
+        bonusText.text = bonus.ToString() + "$";
+    }
+	
+	
+}
