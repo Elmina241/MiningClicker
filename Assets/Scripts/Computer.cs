056b160db54a6bf4c5cd40aaf7d7f7334d102307
Elmina Ross <Elmina Ross>
2017-07-10 09:09:42 +1000

    -

--- Assets/Scripts/Computer.cs
+++ Assets/Scripts/Computer.cs
@@ -7,28 +7,37 @@ class Computer : MonoBehaviour
 {
 
     public Text moneyText;
+    public Text currencyText;
     public Text progressText;
     public Text bonusText;
     public Text expText;
     public Image p1;
     public Image p2;
     public GameObject improvementWin;
-    private int money = 0;
+    public Button bonusButton;
+    private float money = 0;
     int progressCounter1 = 0;
     int progressCounter2 = 0;
+
+    float currency = 0;
+    float exchRate = 200;
+    string currencyName = "ETH";
+
 
     int cost; // цена компьютера
     string nameComp; // Имя компьютера
     bool isReady; // готов ли компьютер к работе (все ли компоненты куплены)
     bool isResearched; // открыт ли компьютер
-    int bonus = 1; // доход за заполнение прогресс-бара
-    int bonusCost = 2; // цена улучшения (на которую поднимаем доход)
+    float bonus = 0.001f; // доход за заполнение прогресс-бара
+    float bonusCost = 2; // цена улучшения (на которую поднимаем доход)
+    float bonusD = 1.32f;
+    float bonusCostD = 1.44f;
     int maxClick=10; // кол-во дробей прогресс-бара
     int exp = 0;
     int expD = 2; // кол-во опыта за клик по кнопке
     int expU = 15; // кол-во опыта за улучшение доходности
     int upgradeCost=200; //максимальное число EXP. Начало с 200
-    float upgradeCoef = 1.12f; // коеф. сложности upgradeCost, для ноута - 1,12. 
+    float upgradeCoef = 1.16f; // коеф. сложности upgradeCost, для ноута - 1,12. 
     int upgradePoints=0; // кол-во очков улучшений (ОУ)
 
     /*-- появляется после покупки автомайна --*/
@@ -37,11 +46,17 @@ class Computer : MonoBehaviour
                           /*!-- появляется после покупки автомайна --*/
 
     int clickCounter = 0; // Счетчик кликов по кнопке
-    int level; // Уровень развития компьютера. Повышается на +1 каждые 50 кликов. Не зависит от EXP 
+    int level = 0; // Уровень развития компьютера. Повышается на +1 каждые 50 кликов. Не зависит от EXP 
     int levelCost; // Цена уровня (кол-во кликов)
 
     AutoMiner autoMiner;
     string jsonParts;
+
+    public void Update()
+    {
+        currencyText.text = currency.ToString("#0.###0");
+        bonusButton.interactable = (money >= bonusCost);
+    }
 
     public void OnClick()
     {
@@ -52,15 +67,17 @@ class Computer : MonoBehaviour
         if (progressCounter1 > maxClick)
         {
             progressCounter1 = 0;
-            money += bonus;
-            moneyText.text = money.ToString();
+            currency += bonus;
         }
         if (progressCounter2 > upgradeCost)
         {
+            level++;
+            if (maxClick > 1) maxClick--;
             progressCounter2 = progressCounter2 % upgradeCost;
             upgradePoints++;
+            upgradeCost = (int)(upgradeCost * upgradeCoef);
         }
-        expText.text = exp.ToString() + "/" + upgradeCost.ToString()+"xp";
+        expText.text = progressCounter2.ToString() + "/" + upgradeCost.ToString()+"xp";
         p1.fillAmount = (float)progressCounter1 / (float)maxClick;
         p2.fillAmount = (float)progressCounter2 / (float)upgradeCost;
         progressText.text = ((int)(((float)progressCounter1 / (float)maxClick) * 100)).ToString() + "%";
@@ -68,15 +85,22 @@ class Computer : MonoBehaviour
     public void GetBonus()
     {
         exp += expU;
-        expText.text = exp.ToString() + "/" + upgradeCost.ToString();
+        expText.text = progressCounter2.ToString() + "/" + upgradeCost.ToString();
         money = money - bonusCost;
-        bonus = bonus + bonusCost;
-        moneyText.text = money.ToString();
-        bonusText.text = bonus.ToString() + "$";
+        moneyText.text = money.ToString() + "$";
+        bonusCost = bonusCost * bonusCostD;
+        bonus = bonus * bonusD;
+        bonusText.text = (bonusCost).ToString("0.#0") + "$";
     }
     public void openCloseImprovementWin()
     {
         improvementWin.SetActive(!improvementWin.activeSelf);
+    }
+    public void Exchange()
+    {
+        money = (money + currency * exchRate);
+        currency = 0;
+        moneyText.text = money.ToString() + "$";
     }
 }
 
