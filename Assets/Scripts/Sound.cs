504304808fdc53cd5ae412e2ac355ef36ae40e73
Just <justtardis@gmail.com>
2017-10-07 23:28:02 +1000

    [ Issue 59 ]

---
+++ Assets/Scripts/Sound.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Sound : MonoBehaviour {
+
+    [SerializeField]
+    private bool _sound = true;
+    public Text textBtn;
+	
+    public void SoundChange()
+    {
+        if (_sound)
+        {
+            _sound = false;
+            textBtn.text = "ВЫКЛ";
+            //Отключаем AudioSourse 
+        }
+        else
+        {
+            _sound = true;
+            textBtn.text = "ВКЛ";
+        }
+            
+    }
+	
+}
