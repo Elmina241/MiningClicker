fd14d05798ddee5ce04b6e69b138ae786d11a65f
Just <justtardis@gmail.com>
2018-02-02 00:25:01 +1000

    Music

--- Assets/Scripts/Music.cs
+++ Assets/Scripts/Music.cs
@@ -3,10 +3,50 @@ using System.Collections;
 
 public class Music : MonoBehaviour
 {
+    /* Нормальный код */
+    private bool clickMusic = false; // булл на проверку клика
+    private bool clickSounds = false; // булл на проверку клика
+
+    private bool currentStateMusic = true; // булл на проверку вкл/выкл музыки
+    private bool currentStateSounds = true; // булл на проверку вкл/выкл звуков
+
+    Vector3 startPos; // начальное положение маркера 
+    Vector3 endPos; // конечное положение маркера
+
+    void Start()
+    {
+        if (currentStateMusic.GetHashCode() == 1)
+        {
+            PlayerPrefs.SetInt("MusicState", currentStateMusic.GetHashCode());
+
+        }
+        else
+        {
+            PlayerPrefs.SetInt("MusicState", currentStateMusic.GetHashCode());
+        }
+        if (currentStateSounds.GetHashCode() == 1)
+        {
+            PlayerPrefs.SetInt("SoundState", currentStateSounds.GetHashCode());
+        }
+        else
+        {
+            PlayerPrefs.SetInt("SoundState", currentStateSounds.GetHashCode());
+        }
+    }
+
+    void SetPosition(string component, int state)
+    {
+        if(component == "music")
+        {
+
+        }
+    }
+
+    /* Нормальный код */
     public Game g;
-    Vector3 startPos, endPos;
+    //Vector3 startPos, endPos;
     int music;
-    public bool click = false;
+    //public bool click = false;
     float time = 0.02f;
     float currentTime = 0f;
     public Color On;
@@ -34,35 +74,34 @@ public class Music : MonoBehaviour
         distance = (endPos - startPos).magnitude;
         print(distance);
     }
-    private void Start()
-    {
-        setUp();
-    }
+    //private void Start()
+    //{
+    //    setUp();
+    //}
 
-    public void Switch()
-    {
-        setUp();
-        click = true;
-        StartCoroutine(Move());
-    }
+    //public void Switch()
+    //{
+    //    setUp();
+    //    click = true;
+    //    StartCoroutine(Move());
+    //}
 
-    IEnumerator Move()
-    {
-        while (click)
-        {
-            yield return new WaitForSeconds(time);
-            currentTime += time;
-            if (currentDist >= distance)
-            {
-                click = false;
-                StopCoroutine(Move());
-            }
-            currentDist = 400f * currentTime;
-            float Perc = currentDist / distance;
-            transform.GetChild(0).transform.localPosition = Vector3.Lerp(startPos, endPos, Perc);
+    //IEnumerator Move()
+    //{
+    //    while (click)
+    //    {
+    //        yield return new WaitForSeconds(time);
+    //        currentTime += time;
+    //        if (currentDist >= distance)
+    //        {
+    //            click = false;
+    //            StopCoroutine(Move());
+    //        }
+    //        currentDist = 400f * currentTime;
+    //        float Perc = currentDist / distance;
+    //        transform.GetChild(0).transform.localPosition = Vector3.Lerp(startPos, endPos, Perc);
 
-        }        
-    }
+    //    }        
+    //}
 
-   
 }
---
+++ Assets/Scripts/NightResScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NightResScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        //gameObject.transform.GetChild(0).GetChild(3).GetComponent<Text>().text = LangSystem.lng.windows[2];
+        //gameObject.transform.GetChild(0).GetChild(2).GetChild(2).GetChild(0).GetComponent<Text>().text = LangSystem.lng.windows[3];
+        //gameObject.transform.GetChild(0).GetChild(2).GetChild(0).GetComponent<Text>().text = LangSystem.lng.windows[4];
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
